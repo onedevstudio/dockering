61775a06da344356ffd41d5b99a5214e35f53b4b
Fernando Moreira <nandomoreira.me@gmail.com>
2017-10-06 21:50:20 -0300

    config dev

--- configs/wp-config.dev.php
+++ configs/wp-config.dev.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_HOST', 'mysqldb');
-define('DB_NAME', 'wp-docker');
-define('DB_USER', 'wp-docker');
-define('DB_PASSWORD', 'wp-docker');
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wordpress');
+define('DB_PASSWORD', 'wordpress');
 
 define('WP_DEBUG', true);
