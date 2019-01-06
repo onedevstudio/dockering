d4b6cdc9bf32bb6fcbbd84932409415dbf812cb9
Fernando Moreira <nandomoreira.me@gmail.com>
2019-01-06 17:31:07 -0200

    add header and footer

--- src/wordpress/wp-content/themes/dockering/index.php
+++ src/wordpress/wp-content/themes/dockering/index.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-		<?php wp_head(); ?>
-	</head>
-	<body <?php body_class() ?>>
-		<div id="site">
-			<welcome title="<?= $theme->get('Name'); ?>" description="<?= $theme->get('Description'); ?>"></welcome>
-		</div>
-		<?php wp_footer(); ?>
-	</body>
-</html>
+<?php get_header(); ?>
+
+	<welcome title="<?= $theme->get('Name'); ?>" description="<?= $theme->get('Description'); ?>"></welcome>
+
+<?php get_footer();
